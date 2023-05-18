d6a0873f6210b115f79ab63f95c62f1c032623f4
manuel1810 <manuelpr1810@gmail.com>
2023-05-17 21:34:30 -0300

    .

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,16 +1,16 @@
-
+import styles from "./About.module.css"
 
 
 export default function About () {
 
     return (
-        <div>
-                <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                     Facere corrupti deserunt, non quos aspernatur quibusdam nobis 
-                     quidem commodi, modi ipsam dolore possimus! Veritatis a 
-                     inventore nostrum suscipit saepe fuga veniam.
-                </p>
+        <div className={styles.abaoutdiv}>
+            <h1>Created by Manuel Penagos</h1>
+            <h2>Full Stack Developer</h2>
+            <h3>Rick and morty app</h3>
+                <p>This app was created for the purpose of practicing 
+                the React skills and apply all the knowledge 
+                obtained in module 2 of the soyHENRY Bootcamp</p>
         </div> 
     )
 
