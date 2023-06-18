3e948c7329357d0309a6b1c77146993a71bc2bac
manuel1810 <manuelpr1810@gmail.com>
2023-06-18 16:42:57 -0300

    hw promises

--- Server/src/controllers/getCharById.js
+++ Server/src/controllers/getCharById.js
@@ -1,8 +1,18 @@
-
+const axios = require("axios");
 
 const getCharById = (res, id) =>{
+axios.get(`https://rickandmortyapi.com/api/character/${id}`)
+    .then(({ data })=>{
+        const{ name, gender, species, origin, image, status} = data;
+        const character = {id, name, gender, species, origin, image, status }
+    
+        res.writeHead(200, {"Content-type": "application/json"})
+            return res.end(JSON.stringify(character))
+    }).catch((err)=>{
+        res.writeHead(500, {"Content-type": "text-plain"})
+            return res.end(err.message)
 
+    })
 
 }
-
-module.export
+module.exports = getCharById;
