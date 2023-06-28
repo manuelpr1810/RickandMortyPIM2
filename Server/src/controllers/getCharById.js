eda214ee22431f5f71580e28c87f41c523ab0d9c
manuel1810 <manuelpr1810@gmail.com>
2023-06-28 14:07:40 -0300

    hw 07 testing finish

--- Server/src/controllers/getCharById.js
+++ Server/src/controllers/getCharById.js
@@ -14,7 +14,7 @@ try {
         res.status(200).json(character) : res.status(404).send("Not found")
     }
 } catch (error) {
-    return  res.status(500).send(message.error)
+    return  res.status(500).send(error.message)
 }
 };
 
